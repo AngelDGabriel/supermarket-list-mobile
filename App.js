00449478dcdb4fb2dc6d3de61f2e67ea6eea71a2
AngelDGabriel <angelkengms@gmail.com>
2023-12-10 22:54:34 -0300

    feat: custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { NavigationContainer } from "@react-navigation/native"
-import { Router } from "./src/router"
+import { useCallback } from 'react'
+import { View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from 'expo-font'
+import { Router } from './src/router'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'AvenirNext-DemiBold': require('./assets/fonts/AvenirNext-DemiBold.ttf'),
+    'AvenirNext-Bold': require('./assets/fonts/AvenirNext-Bold.ttf'),
+    'AvenirNext-Medium': require('./assets/fonts/AvenirNext-Medium.ttf'),
+    'AvenirNext-Regular': require('./assets/fonts/AvenirNext-Regular.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </View>
   )
 }
